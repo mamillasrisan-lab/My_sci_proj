27af73529eff95b5ca1fa9bc746b40425b31074a
mamillasrisan-lab <mamillasrisan@gmail.com>
2025-12-23 11:58:21 -0800

    Update Preset_image_Website.py

--- Preset_image_Website.py
+++ Preset_image_Website.py
@@ -9,7 +9,7 @@ from transformers import BlipForConditionalGeneration, AutoProcessor
 # PAGE CONFIG
 # ===============================
 st.set_page_config(
-    page_title="BLIP Image Captioning",
+    page_title="Image Identification and Captioning",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
