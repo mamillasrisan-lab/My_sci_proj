8e0e59518c8f8a2947c81f71d0167e213224fa04
mamillasrisan-lab <mamillasrisan@gmail.com>
2025-12-22 16:12:12 -0800

    Update Preset_image_Website.py

--- Preset_image_Website.py
+++ Preset_image_Website.py
@@ -40,7 +40,7 @@ processor, model = load_blip()
 # PRESET IMAGES (GITHUB RAW)
 # -------------------------------
 PRESETS = {
-    "Fruit Flies in Farms": "https://raw.githubusercontent.com/mamillasrisan-lab/Images/refs/heads/main/FF/fruit_flies_in_farms_161.jpg",
+    "Fruit Flies in Farms": "https://raw.githubusercontent.com/mamillasrisan-lab/Images/refs/heads/main/FF/fruit_flies_in_farms_135.jpg",
     "Audi Car": "https://raw.githubusercontent.com/mamillasrisan-lab/Images/refs/heads/main/CAR/cars_1.jpg",
     "Historical Exhibit Room": "https://raw.githubusercontent.com/mamillasrisan-lab/Images/refs/heads/main/Exhibit/Historical_Exhibit_room_177.jpg",
     "Household Objects": "https://raw.githubusercontent.com/mamillasrisan-lab/Images/refs/heads/main/HO/House_hold_objects_156.jpg",
